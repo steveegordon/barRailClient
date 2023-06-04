d2fcf620b1e9d9858f3cc9b0921014b671de21d5
stevegordon <tdtsgordon@gmail.com>
2023-06-03 23:59:43 -0700

    add data component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { UserComponent } from './components/user/user.component';
+import { DataComponent } from './components/data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    UserComponent
+    UserComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
