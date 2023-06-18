691bb084f02cea54f5a4e31c8271656de384c50d
stevegordon <tdtsgordon@gmail.com>
2023-06-18 00:00:17 -0700

    socket.io

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -14,8 +14,16 @@ export class DataService {
 
   constructor( private http: HttpClient, private socket: Socket) { }
 
+  sendMessage(msg: String) {
+    this.socket.emit('sending', msg)
+  }
+  receiveUpdate() {
+    return this.socket.fromEvent('connected').pipe(console.log(data));
+  }
+
   postData(data: Data): Observable<Data> {
     console.log(data);
+    this.sendMessage();
     return this.http.post<Data>(Constants.DATA_ENDPOINT, data);
   }
 
